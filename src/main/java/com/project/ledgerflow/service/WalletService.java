9c597e38fda2dcbe8a4875ed91223a0557219cef
Ayush <deoreayush555@gmail.com>
2026-04-09 21:35:48 +0530

    implement core wallet service, DTOs, and REST API

---
+++ src/main/java/com/project/ledgerflow/service/WalletService.java
@@ -0,0 +1,63 @@
+package com.project.ledgerflow.service;
+
+import com.project.ledgerflow.model.Wallet;
+import com.project.ledgerflow.repository.WalletRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    @Transactional
+    public Wallet createWallet(String currency){
+        Wallet wallet = WalletBuilder()
+                .balance(BigDecimal.ZERO)
+                .currency(currency.toUpperCase())
+                .build();
+        return walletRepository.save(wallet);
+    }
+
+    @Transactional(readOnly = true)
+    public Wallet getWallet(UUID walletId){
+        return walletRepository.findById(walletId)
+                .orElseThrow(() -> new RuntimeException("Wallet not found with ID: " + walletId));
+    }
+
+    @Transactional
+    public Wallet credit(UUID walletId, BigDecimal amount){
+        if(amount.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("Credit amount must be greater than zero");
+        }
+
+        Wallet wallet = getWallet(walletId);
+        wallet.setBalance(wallet.getBalance().add(amount));
+
+        return walletRepository.save(wallet);
+    }
+
+    @Transactional
+    public Wallet debit(UUID walletId,  BigDecimal amount){
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Debit amount must be greater than zero");
+        }
+
+        Wallet wallet = getWallet(walletId);
+
+        // Check for sufficient funds
+        if (wallet.getBalance().compareTo(amount) < 0) {
+            throw new RuntimeException("Insufficient funds. Current balance: " + wallet.getBalance());
+        }
+
+        wallet.setBalance(wallet.getBalance().subtract(amount));
+
+        return walletRepository.save(wallet);
+    }
+
+}
